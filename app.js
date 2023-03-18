8090840138418061b63b05819dfaaf2ee58f3f2b
elvis-visi <sivisevi1@gmail.com>
2023-03-18 17:53:45 +0000

    2. Display each book on the page.

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ Book.prototype.info = function () {
 const book = new Book("My Book","sevi",500,true)
 console.log(book.info())
 console.log("book: ", book)
+const bookTwo = new Book("Lezha","Riku",200,false)
+const bookThree = new Book("Tirana","sevi",120,true)
+myLibrary.push(book)
+myLibrary.push(bookTwo)
+myLibrary.push(bookThree)
+console.log("library: ",myLibrary)
+
 
 function addBookToLibrary() {
   //take user’s input and store the new book objects into an array.
@@ -31,5 +38,37 @@ function addBookToLibrary() {
 
 }
 
+function displayBooks(){
+    const mainDiv = document.querySelector(".main");
+
+    //create a div(card) for each book in the library
+    for(let book of myLibrary)
+    {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const title = document.createElement("h2");
+        title.textContent = book.title;
+        card.appendChild(title);
+
+        let author = document.createElement("p");
+        author.textContent = `Author: ${book.author}`;
+        card.appendChild(author);
+
+        let pages = document.createElement("p");
+        pages.textContent = `Pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        let isRead = document.createElement("p");
+        isRead.textContent = book.isRead ? "Read" : "Not read yet";
+        card.appendChild(isRead);
+
+        mainDiv.appendChild(card);
+    }
+
+
+}
+
+displayBooks()
 //addBookToLibrary()
 
